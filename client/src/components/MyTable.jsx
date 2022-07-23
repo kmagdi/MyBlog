70bd4dfd9383de2016da208bc68a1d181d56de1f
KMagdi <46417302+kmagdi@users.noreply.github.com>
2022-07-23 21:19:48 +0200

    seo

--- client/src/components/MyTable.jsx
+++ client/src/components/MyTable.jsx
@@ -1,13 +1,55 @@
 import React from 'react'
-import { useTable,useSortBy } from 'react-table'
+import { useTable,useSortBy,useFilters } from 'react-table'
+
+// Define a default UI for filtering
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
+
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  )
+}
+
 export const MyTable=({columns,data})=> {
+
+  const filterTypes = React.useMemo(
+    () => ({
+       text: (rows, id, filterValue) => {
+        return rows.filter(row => {
+          const rowValue = row.values[id]
+          return rowValue !== undefined  ? String(rowValue).toLowerCase().startsWith(String(filterValue).toLowerCase())  : true
+        })
+      },
+    }),[])
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  )
+
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
     rows,
-    prepareRow,
-  } = useTable({ columns, data },useSortBy)
+    prepareRow
+  } = useTable({ columns, data ,defaultColumn,filterTypes},useFilters, useSortBy)
+
+  // We don't want to render all of the rows for this example, so cap
+  // it for this use case
+  const firstPageRows = rows.slice(0, 10)
+
 
   return (
     <div className="container">
@@ -29,9 +71,11 @@ export const MyTable=({columns,data})=> {
               >
                 {column.render('Header')}
                 {/* Add a sort direction indicator */}
-                <span>
+                 <span>
                     {column.isSorted ? column.isSortedDesc ? ' 🔽' : ' 🔼' : '🪄'}
                   </span>
+                  {/* Render the columns filter UI */}
+                 <div>{column.canFilter ? column.render('Filter') : null}</div>
               </th>
             ))}
           </tr>
